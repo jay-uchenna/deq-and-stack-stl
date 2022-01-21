92aad7ccf63919fa0510a9fb5864eba6def79f03
Ezeobi Uchenna <78647544+jay-uchenna@users.noreply.github.com>
2022-01-21 17:24:52 +0300

    Initial Commit

---
+++ deq and stack stl/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  deq and stack stl
+//
+//  Created by Jedidiah Uchenna on 21.01.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
